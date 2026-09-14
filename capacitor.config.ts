d7218b08d985a7e89737b127c4dedea31669e45a
Serhii Hrybanov <shrybanov@sapi-tech.com>
2026-09-14 13:21:10 +0000

    Fix UI5 version and cleartext permissions

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.mycompany.myapp',
   appName: 'InfoboxMain',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true, // РАЗРЕШАЕМ HTTP запросы для тестов
+    allowNavigation: ['*'] // Разрешаем навигацию на любые домены
+  }
 };
 
 export default config;
